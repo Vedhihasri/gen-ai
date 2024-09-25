fd23f1765f4b3576b4d291e3b43a67e07c815a9a
Vedhihasri <vedhihasrisrinivasan@gmail.com>
2024-09-25 17:47:37 +0530

    Add files via upload

---
+++ gen.py
@@ -0,0 +1,14 @@
+import PyPDF2
+def extract_text_from_pdf(pdf_path):
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        text = ''
+        for page_num in range(len(reader.pages)):
+            page = reader.pages[page_num]
+            text += page.extract_text()
+    return text
+
+# Example usage
+pdf_path =r"C:\Users\a\Downloads\organic che.pdf"
+text = extract_text_from_pdf(pdf_path)
+print(text)
